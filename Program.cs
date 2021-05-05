511c48a764fffb8b6f632a34f671e314349a95d6
Abhijit Jana <Abhijit.Jana@Westpharma.com>
2021-05-05 20:51:15 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Azure.Messaging.WebPubSub;
+using Websocket.Client;
+
+var webPubSubServiceClient = new WebPubSubServiceClient("Endpoint=https://azurepubsubdemo.webpubsub.azure.com;AccessKey=tGXgGGnOSpnCSiVdZIZ+P1L19/YWBjcqNjEf+xEgD/0=;Version=1.0;", "Hub");
+var url = webPubSubServiceClient.GetClientAccessUri();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Connecting with Client....");
+using (var websocketClient = new WebsocketClient(url))
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    websocketClient.MessageReceived.Subscribe(receivedMessage => Console.WriteLine($"Message received: {receivedMessage}"));
+    await websocketClient.Start();
+    Console.WriteLine("Connected.. Waiting for Message");
+    Console.Read();
+}
+
